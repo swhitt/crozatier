14c66d1528f45fabd82d68175e3db151c5843620
Steve Whittaker <swhitt@gmail.com>
2010-09-19 23:44:15 +0200

    French language translations.

--- html/js/crozatier.js
+++ html/js/crozatier.js
@@ -1,23 +1,50 @@
+function getParameterByName(name) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+Crozatier.lang = (getParameterByName('lang') == 'fr') ? 'fr' : 'en';
+
 jQuery(document).ready(function($) {
+  
+  
   var store = new Ext.data.JsonStore({
     autoDestroy: true,
     data: Crozatier,
     storeId: 'crozatierStore',
     root: 'stuff',
     idProperty: 'slug',
-    fields: ['slug', 'name', 'brand', 'model', 'details', 'docs', 'purchase_date', 'original_cost', 'asking_cost', 'delta', 'percent_discount']
+    fields: ['slug', 'name', 'french_name', 'brand', 'model', 'details', 'docs', 'purchase_date', 'original_cost', 'asking_cost', 'delta', 'percent_discount']
   });
-  var grid = new Ext.grid.GridPanel({
-    store: store,
-    columns : [
+  var cols;
+  if(Crozatier.lang == 'en') {
+    cols = [
       {header: 'Name', dataIndex: 'name', width: 110},
       {header: 'Brand', dataIndex: 'brand', width: 75},
       {header: 'Model', dataIndex: 'model', width: 100},
       {header: 'Cost', dataIndex: 'asking_cost', width: 50, align: 'right'}
-    ],
+    ]
+  } else {
+    cols = [
+      {header: 'Nom', dataIndex: 'french_name', width: 110},
+      {header: 'Marque', dataIndex: 'brand', width: 75},
+      {header: 'Modèle', dataIndex: 'model', width: 100},
+      {header: 'Coût', dataIndex: 'asking_cost', width: 50, align: 'right'}
+    ]
+  }
+  var grid = new Ext.grid.GridPanel({
+    store: store,
+    columns : cols,
     sm: new Ext.grid.RowSelectionModel({singleSelect: true}),
     region: 'west',
-    title: 'Item List',
+    title: (Crozatier.lang == 'en') ? 'Item List' : 'Liste des articles',
     collapsible: true,
     titleCollapse: true,
     floatable: false,
@@ -33,19 +60,37 @@ jQuery(document).ready(function($) {
       Ext.getCmp(tabName).show();
     } else {
 
-      var stuffTplMarkup = [
-        '<h1>{name}</h1>',
-        '<p>{details}</p>',
-        '<table class="sell-figs">',
-        '<tr class="even"><td>Brand</td><td>{brand}</td></tr>',
-        '<tr class="odd"><td>Model</td><td>{model}</td></tr>',
-        '<tr class="even"><td>Have docs?</td><td>{docs}</td></tr>',
-        '<tr class="odd"><td>Purchase Date</td><td>{purchase_date}</td></tr>',
-        '<tr class="even"><td>Original Cost</td><td>{original_cost}</td></tr>',
-        '<tr class="odd"><td>Asking Cost</td><td>{asking_cost}</td></tr>',
-        '</table>',
-        '<div id="{slug}-rest"></div>'
-      ];
+      var stuffTplMarkup;
+      
+      if(Crozatier.lang == 'en') {
+        stuffTplMarkup = [
+          '<h1>{name}</h1>',
+          '<p>{details}</p>',
+          '<table class="sell-figs">',
+          '<tr class="even"><td>Brand</td><td>{brand}</td></tr>',
+          '<tr class="odd"><td>Model</td><td>{model}</td></tr>',
+          '<tr class="even"><td>Have docs?</td><td>{docs}</td></tr>',
+          '<tr class="odd"><td>Purchase Date</td><td>{purchase_date}</td></tr>',
+          '<tr class="even"><td>Original Cost</td><td>{original_cost}</td></tr>',
+          '<tr class="odd"><td>Asking Cost</td><td>{asking_cost}</td></tr>',
+          '</table>',
+          '<div id="{slug}-rest"></div>'
+        ];
+      } else {
+        stuffTplMarkup = [
+          '<h1>{french_name}</h1>',
+          '<p>{details}</p>',
+          '<table class="sell-figs">',
+          '<tr class="even"><td>Marque</td><td>{brand}</td></tr>',
+          '<tr class="odd"><td>Modèle</td><td>{model}</td></tr>',
+          '<tr class="even"><td>Documentation?</td><td>{docs}</td></tr>',
+          '<tr class="odd"><td>Date d\'achat</td><td>{purchase_date}</td></tr>',
+          '<tr class="even"><td>Le coût initial</td><td>{original_cost}</td></tr>',
+          '<tr class="odd"><td>Coût</td><td>{asking_cost}</td></tr>',
+          '</table>',
+          '<div id="{slug}-rest"></div>'
+        ];
+      }
 
       var stuffTpl = new Ext.Template(stuffTplMarkup);
 
@@ -59,7 +104,7 @@ jQuery(document).ready(function($) {
       
       var restEl = $('#'+r.data.slug+'-rest');
       $.ajax({
-        url: 'details/'+r.data.slug+'.html',
+        url: 'details/'+r.data.slug+'-'+Crozatier.lang+'.html',
         dataType: 'html',
         success: function(data) {
           restEl.html(data);
@@ -83,7 +128,7 @@ jQuery(document).ready(function($) {
       collapsible: true,
       floatable: false,
       titleCollapse: true,
-      contentEl: 'information-text',
+      contentEl: 'information-text-'+Crozatier.lang,
       id: 'information-panel',
       split: true
     }, 
@@ -95,12 +140,18 @@ jQuery(document).ready(function($) {
       activeTab: 0,
       enableTabScroll:true,
       items: {
-        title: 'Key Information',
-        contentEl: 'key-information',
+        title: (Crozatier.lang == 'en') ? 'Key Information' : 'Informations Clés',
+        contentEl: 'key-information-'+Crozatier.lang,
         id: 'key-information-tab'
       }
     }]
   });
+  
+  if(Crozatier.lang == 'en') {
+    $('.french').hide();
+  } else {
+    $('.english').hide();
+  }
 
 });
 
